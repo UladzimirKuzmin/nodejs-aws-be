310aaefdb60c1b2962b0de39cd0f6e93c0e890c3
vladimirk <specialkv@gmail.com>
2021-08-23 00:54:13 +0200

    updated getProductById handler

--- product-service/src/functions/getProductById/handler.ts
+++ product-service/src/functions/getProductById/handler.ts
@@ -1,14 +1,19 @@
 import 'source-map-support/register';
 
 import type { APIGatewayProxyEvent, APIGatewayProxyResult, Handler } from 'aws-lambda';
+import { getProductsMock } from '@libs/s3';
 import { formatJSONResponse } from '@libs/apiGateway';
 import { middyfy } from '@libs/lambda';
 
 const getProductById: Handler<APIGatewayProxyEvent, APIGatewayProxyResult> = async (event) => {
   const { id } = event.pathParameters;
-  return formatJSONResponse({
-    id,
-  });
+
+  const json = await getProductsMock();
+  const products = JSON.parse(json);
+
+  const product = products?.data.find((entry) => entry.id === Number(id));
+
+  return formatJSONResponse(product ?? {});
 };
 
 export const main = middyfy(getProductById);
