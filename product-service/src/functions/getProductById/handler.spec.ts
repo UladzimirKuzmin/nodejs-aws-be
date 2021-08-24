3ea230466e776c672b4e8729fb71d7a3ed1624a0
vladimirk <specialkv@gmail.com>
2021-08-24 13:48:17 +0200

    fixed tests

--- product-service/src/functions/getProductById/handler.spec.ts
+++ product-service/src/functions/getProductById/handler.spec.ts
@@ -1,21 +1,20 @@
-import * as fs from 'fs';
-import * as AWSMock from 'aws-sdk-mock';
 import context from 'aws-lambda-mock-context';
 import { Callback } from 'aws-lambda';
+import { getProductsMock } from '@libs/s3';
 import { main as getProductById } from './handler';
 
 import mock from './mock.json';
 
-AWSMock.mock('S3', 'getObject', Buffer.from(fs.readFileSync(`${__dirname}/mock.json`, 'utf-8')));
+jest.mock('@libs/s3', () => ({
+  getProductsMock: jest.fn(),
+}));
 
 const event = { pathParameters: { id: '1580' } };
 const ctx = context();
 const callback: Callback = () => {};
 
 describe('getProductsList', () => {
-  afterEach(() => {
-    AWSMock.restore('S3');
-  });
+  (getProductsMock as jest.Mock).mockResolvedValue(JSON.stringify({ data: [mock] }));
 
   test('Returns empty object in body', async () => {
     const result = await getProductById({ pathParameters: { id: '999' } }, ctx, callback);
@@ -24,7 +23,6 @@ describe('getProductsList', () => {
 
   test('Returns product object in body', async () => {
     const result = await getProductById(event, ctx, callback);
-    console.log(result);
     expect(result).toEqual({ statusCode: 200, body: JSON.stringify(mock) });
   });
 });
--- product-service/src/functions/getProductsList/handler.spec.ts
+++ product-service/src/functions/getProductsList/handler.spec.ts
@@ -1,23 +1,21 @@
-import * as fs from 'fs';
-import * as AWSMock from 'aws-sdk-mock';
 import context from 'aws-lambda-mock-context';
 import { Callback } from 'aws-lambda';
+import { getProductsMock } from '@libs/s3';
 import { main as getProductsList } from './handler';
 
 import mock from './mock.json';
 
-AWSMock.mock('S3', 'getObject', Buffer.from(fs.readFileSync(`${__dirname}/mock.json`, 'utf-8')));
+jest.mock('@libs/s3', () => ({
+  getProductsMock: jest.fn(),
+}));
 
 const event = {};
 const ctx = context();
 const callback: Callback = () => {};
 
 describe('getProductsList', () => {
-  afterEach(() => {
-    AWSMock.restore('S3');
-  });
-
   test('Returns array of products', async () => {
+    (getProductsMock as jest.Mock).mockResolvedValue(JSON.stringify(mock));
     const result = await getProductsList(event, ctx, callback);
     expect(result).toEqual({ statusCode: 200, body: JSON.stringify(mock) });
   });
