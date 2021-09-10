7cff7ecf4dcc735af36052505b03a0f6b41a2ea7
vladimirk <specialkv@gmail.com>
2021-09-11 00:24:27 +0200

    config env update

--- product-service/serverless.ts
+++ product-service/serverless.ts
@@ -1,7 +1,4 @@
 import type { AWS } from '@serverless/typescript';
-import { config } from 'dotenv';
-
-config();
 
 import getProductsList from '@functions/getProductsList';
 import getProductById from '@functions/getProductById';
@@ -29,11 +26,11 @@ const serverlessConfiguration: AWS = {
     },
     environment: {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
-      PG_HOST: process.env.PG_HOST,
-      PG_PORT: process.env.PG_PORT,
-      PG_DATABASE: process.env.PG_DATABASE,
-      PG_USERNAME: process.env.PG_USERNAME,
-      PG_PASSWORD: process.env.PG_PASSWORD,
+      PG_HOST: '${env:PG_HOST}',
+      PG_PORT: '${env:PG_PORT}',
+      PG_DATABASE: '${env:PG_DATABASE}',
+      PG_USERNAME: '${env:PG_USERNAME}',
+      PG_PASSWORD: '${env:PG_PASSWORD}',
     },
     lambdaHashingVersion: '20201221',
   },
