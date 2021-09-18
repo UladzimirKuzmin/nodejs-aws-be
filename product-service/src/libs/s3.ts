6e1a514b1aa7f5fa8bab822b12b066f833d6b360
vladimirk <specialkv@gmail.com>
2021-09-18 09:07:08 +0200

    added try catch for s3 service

--- product-service/src/libs/s3.ts
+++ product-service/src/libs/s3.ts
@@ -5,8 +5,12 @@ config.update({ region: 'eu-west-1' });
 const s3 = new S3();
 
 export const getProductsMock = async () => {
-  const response = await s3
-    .getObject({ Bucket: 'nodejs-aws-be-mocks', Key: 'products.json' })
-    .promise();
-  return response.Body.toString('utf-8');
+  try {
+    const response = await s3
+      .getObject({ Bucket: 'nodejs-aws-be-mocks', Key: 'products.json' })
+      .promise();
+    return response.Body.toString('utf-8');
+  } catch (error) {
+    return error;
+  }
 };
