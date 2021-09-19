cb952f83c35bb4035585f15061e796af20a30cf7
vladimirk <specialkv@gmail.com>
2021-09-19 14:45:25 +0200

    import service config

---
+++ import-service/serverless.ts
@@ -0,0 +1,33 @@
+import type { AWS } from "@serverless/typescript";
+
+import importProductsFile from "@functions/importProductsFile";
+
+const serverlessConfiguration: AWS = {
+  service: "import-service",
+  frameworkVersion: "2",
+  custom: {
+    webpack: {
+      webpackConfig: "./webpack.config.js",
+      includeModules: true,
+    },
+  },
+  useDotenv: true,
+  plugins: ["serverless-webpack"],
+  provider: {
+    name: "aws",
+    runtime: "nodejs14.x",
+    region: "eu-west-1",
+    stage: "dev",
+    apiGateway: {
+      minimumCompressionSize: 1024,
+      shouldStartNameWithService: true,
+    },
+    environment: {
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
+    },
+    lambdaHashingVersion: "20201221",
+  },
+  functions: { importProductsFile },
+};
+
+module.exports = serverlessConfiguration;
