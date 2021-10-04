1da7d4392b2ce38d8968fa758ecae2d08fcd6c04
vladimirk <specialkv@gmail.com>
2021-10-04 23:33:57 +0200

    fixed tests

--- product-service/src/functions/getProductById/handler.spec.ts
+++ product-service/src/functions/getProductById/handler.spec.ts
@@ -1,26 +1,34 @@
+import { Client } from 'pg';
 import { Callback, Context } from 'aws-lambda';
-import { getProductsMock } from '@libs/s3';
 import { main as getProductById } from './handler';
 
 import mock from './mock.json';
 
-jest.mock('@libs/s3', () => ({
-  getProductsMock: jest.fn(),
-}));
+jest.mock('pg', () => {
+  const mClient = { connect: jest.fn(), query: jest.fn(), end: jest.fn() };
+  return {
+    Client: jest.fn(() => mClient),
+  };
+});
 
 const event = { pathParameters: { id: '1580' } };
 const ctx = {} as Context;
 const callback: Callback = () => {};
 
 describe('getProductById', () => {
-  (getProductsMock as jest.Mock).mockResolvedValue(JSON.stringify({ data: [mock] }));
-
-  test.skip('Returns statusCode 404', async () => {
-    const result = await getProductById({ pathParameters: { id: '999' } }, ctx, callback);
-    expect(result).toEqual({ statusCode: 404 });
+  let mockedClient: jest.Mocked<Client>;
+  beforeAll(() => {
+    mockedClient = new Client({}) as any;
   });
 
-  test.skip('Returns product object in body', async () => {
+  test('Returns statusCode 404', async () => {
+    mockedClient.query.mockResolvedValue({ rows: [] } as never);
+    const result = await getProductById({ pathParameters: { id: '999' } }, ctx, callback);
+    expect(result.statusCode).toEqual(404);
+  });
+
+  test('Returns product object in body', async () => {
+    mockedClient.query.mockResolvedValue({ rows: [mock] } as never);
     const result = await getProductById(event, ctx, callback);
     expect(result).toEqual({ statusCode: 200, body: JSON.stringify(mock) });
   });
