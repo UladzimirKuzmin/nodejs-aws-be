2302896d7d357c1cddb1a5a5c312b5bb55e99579
vladimirk <specialkv@gmail.com>
2021-10-04 18:39:21 +0200

    added reusable create product function

--- product-service/src/functions/catalogBatchProcess/handler.ts
+++ product-service/src/functions/catalogBatchProcess/handler.ts
@@ -1,9 +1,22 @@
 import 'source-map-support/register';
 
+import { SQSEvent } from 'aws-lambda';
+import { create } from '@libs/db';
 import { middyfy } from '@libs/lambda';
+import { formatJSONResponse } from '@libs/apiGateway';
 
-const catalogBatchProcess = async (event) => {
-  console.log(event);
+const catalogBatchProcess = async (event: SQSEvent) => {
+  console.info(event);
+
+  try {
+    for (let record of event.Records) {
+      await create(JSON.parse(record.body));
+    }
+
+    return formatJSONResponse({ message: 'SUCCESS' }, 200);
+  } catch (error) {
+    return formatJSONResponse(error, 500);
+  }
 };
 
 export const main = middyfy(catalogBatchProcess);
