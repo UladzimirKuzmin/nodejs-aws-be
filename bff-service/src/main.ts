f1b0e834b22f19e18ae34986c3df856849b179b4
vladimirk <specialkv@gmail.com>
2021-11-09 22:10:30 +0100

    feat: added cors

--- bff-service/src/main.ts
+++ bff-service/src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 dotenv.config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   await app.listen(process.env.PORT ? process.env.PORT : 3000);
 }
 bootstrap();
